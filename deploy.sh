bdd0a9f6ef3f35128592abd916c1f422d3e37279
Novak Boškov <gnovak.boskov@gmail.com>
2017-03-23 13:05:34 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env sh
 
+git checkout develop
+
 stack build
 git submodule update
 stack exec site build
 cp README.md _site/README.md
-cd _site && git add -A && git commit -m "publish" && git push origin master --force
+git add -A && git commit -m "deploy"
+
+# git checkout master
+# git merge develop -X theirs
+
+# shopt -s extglob
+# rm -r !(.git|_site)
+# cp _site/* .
+# rm -r _site
+
+# git add -A && git commit -m "publish" && git push
+
+git checkout develop
