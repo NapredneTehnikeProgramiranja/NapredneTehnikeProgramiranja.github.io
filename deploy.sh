3c7df53c2aa2650b60971fb6a9de4d66a2560951
Novak Boškov <gnovak.boskov@gmail.com>
2017-03-23 12:14:02 +0100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
 
 stack build
-stack exec site clean
 git submodule update
-cd _site && git checkout master && cd ..
 stack exec site build
 cp README.md _site/README.md
 cd _site && git add -A && git commit -m "publish" && git push origin master --force
