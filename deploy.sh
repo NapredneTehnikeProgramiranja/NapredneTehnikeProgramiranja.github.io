9adc700f40940828b9be113b35cfe496cddd31f7
Novak Boškov <gnovak.boskov@gmail.com>
2017-03-23 13:17:54 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,17 @@ stack build
 git submodule update
 stack exec site build
 cp README.md _site/README.md
+cp CNAME _site/CNAME
 git add -A && git commit -m "deploy"
 
-# git checkout master
-# git merge develop -X theirs
+git checkout master
+git merge develop -X theirs
 
-# shopt -s extglob
-# rm -r !(.git|_site)
-# cp _site/* .
-# rm -r _site
+shopt -s extglob
+rm -r !(\.git|\_site|CNAME)
+cp _site/* .
+rm -r _site
 
-# git add -A && git commit -m "publish" && git push
+git add -A && git commit -m "publish" && git push
 
 git checkout develop
