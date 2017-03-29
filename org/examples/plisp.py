b565916b66d3e35c6ddcd6e22faded542057bd6e
Novak Boškov <gnovak.boskov@gmail.com>
2017-03-29 16:41:52 +0200

    Parsing in TextX

--- org/examples/plisp.py
+++
@@ -1,4 +0,0 @@
-from textx.metamodel import metamodel_from_file
-
-mm = metamodel_from_file("plisp.tx")
-m = mm.model_from_file("example.plisp")
---
+++ org/py_examples/plisp.py
@@ -0,0 +1,3 @@
+from textx.metamodel import metamodel_from_file
+
+m = metamodel_from_file("plisp.tx").model_from_file("example.plisp")
